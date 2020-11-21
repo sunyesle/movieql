52936ff10b55d95eb4ab7bd6c07e980aa16f4959
sunyesle <sunyesle@gmail.com>
2020-11-21 17:04:20 +0900

    Creating mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,49 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: 'aaa',
-    age: 15,
-    gender: 'female',
+    name: 'Star Wars - The new one',
+    score: 1,
   },
   {
     id: 1,
-    name: 'bbb',
-    age: 23,
-    gender: 'male',
+    name: 'Avengers - The new one',
+    score: 8,
   },
   {
     id: 2,
-    name: 'ccc',
-    age: 57,
-    gender: 'male',
+    name: 'The Godfather I',
+    score: 99,
   },
   {
     id: 3,
-    name: 'ddd',
-    age: 8,
-    gender: 'female',
-  },
-  {
-    id: 4,
-    name: 'eee',
-    age: 34,
-    gender: 'male',
-  },
-  {
-    id: 5,
-    name: 'fff',
-    age: 41,
-    gender: 'female',
+    name: 'Logan',
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
